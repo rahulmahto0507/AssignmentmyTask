d93517fcfd1d0006b544d45f4f558f363a35a626
rahul <rahulmahto0507@gmail.com>
2023-02-14 05:46:34 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+
+import MyTask from './components/MyTask'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <MyTask />
+
+export default App
